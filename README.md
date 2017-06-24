c52422e8617f460501845cdd9fbe16cc69fb3013
Brad Traversy <me@bradtraversy.com>
2017-06-24 09:38:56 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# ReachCharts App
+
+Example app showing you how to use react-chartjs-2 in a project
+
+## Install Dependencies
+```bash
+npm install 
+```
+
+## Run Dev Server
+```bash
+npm start
+```
+
+## Build To Dist Folder
+```bash
+npm run build
+```
---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Chart from './components/Chart';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      chartData:{}
+    }
+  }
+
+  componentWillMount(){
+    this.getChartData();
+  }
+
+  getChartData(){
+    // Ajax calls here
+    this.setState({
+      chartData:{
+        labels: ['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
+        datasets:[
+          {
+            label:'Population',
+            data:[
+              617594,
+              181045,
+              153060,
+              106519,
+              105162,
+              95072
+            ],
+            backgroundColor:[
+              'rgba(255, 99, 132, 0.6)',
+              'rgba(54, 162, 235, 0.6)',
+              'rgba(255, 206, 86, 0.6)',
+              'rgba(75, 192, 192, 0.6)',
+              'rgba(153, 102, 255, 0.6)',
+              'rgba(255, 159, 64, 0.6)',
+              'rgba(255, 99, 132, 0.6)'
+            ]
+          }
+        ]
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <Chart chartData={this.state.chartData} location="Massachusetts" legendPosition="bottom"/>
+      </div>
+    );
+  }
+}
+
+export default App;
